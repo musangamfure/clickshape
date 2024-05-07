089066f827b23c040bf74d0454d986ed24ea2ec3
Musangamfura Emmanuel <musangamfure@gmail.com>
2024-05-07 17:47:32 +0200

    Project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+
+import { ThemeProvider } from "@/providers/theme-provider";
+
+const inter = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ClickShape",
+  description: "All in one agency solution.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/global/file-upload.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {
+  apiEndpoint: "agencyLogo" | "avatar" | "subaccountLogo";
+};
+
+const FileUpload = () => {
+  return <div className="">FileUpload</div>;
+};
+
+export default FileUpload;
